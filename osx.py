68d74f85d56faa843113d8d6456f8fc612cee1e3
Autumn-one <718879459@qq.com>
2022-01-29 22:17:21 +0800

    添加 shutil

--- osx.py
+++ osx.py
@@ -1,74 +1,74 @@
 from os import walk, listdir, path
 from pathlib import Path
-from pathx import * # 导入所有的路径增强函数
-from walkx import * # 导入所有的递归访问文件夹的方法
+from pathx import *  # 导入所有的路径增强函数
+from walkx import *  # 导入所有的递归访问文件夹的方法
+from pprint import *  # 导入所有的pprint的方法
+from shutil import * # 导入文件的高级操作部分
 
 
-def getFolders(pathStr):
-    """返回给定路径的直接子文件夹"""
-    return [*filter(
-            lambda i: path.isdir(i),
-            [*map(
-                    lambda i: str(Path(pathStr, i)),
-                    listdir(pathStr)
-            )]
-    )]
+def get_dirs(path_str=None, nopath=False):
+    """
+    返回给定路径的直接子文件夹
+    nopath 表示是否输出完整的路径
+    """
+    dirs = []  # 存放最终结果的文件列表
+
+    if not path_str:
+        path_str = cwd()
+
+    for item in listdir(path_str):
+        item_path = Path(path_str, item)
+        if is_dir(item_path):
+            final_path = item if nopath else str(item_path)
+            dirs.append(final_path)
+
+    return dirs
 
 
-def getFiles(pathStr, extension=None):
-    """返回给定路径的直接子文件"""
-    files = [*filter(
-            lambda i: path.isfile(i),
-            [*map(
-                    lambda i: str(Path(pathStr, i)),
-                    listdir(pathStr)
-            )]
-    )]
+def get_files(path_str, extension="", *, nopath=False):
+    """
+    返回给定路径的直接子文件
+    extension 表示后缀名不带点
+    nopath表示是狗返回完整路径,注意不一定是绝对路径
+    """
+    files = []  # 存放最终结果的文件列表
+
+    if not path_str:
+        path_str = cwd()
+
     if extension:
-        files = [*filter(
-                lambda i: i.endswith(f".{extension}"),
-                files
-        )]
+        extension = "." + extension
+
+    for item in listdir(path_str):
+        item_path = Path(path_str, item)
+        if is_file(item_path) and item.endswith(extension):
+            final_path = item if nopath else str(item_path)
+            files.append(final_path)
+
     return files
 
 
-def getFilesAndFolders(pathStr):
-    """返回给定路径的直接文件列表和文件夹列表"""
-    filesAndFolders = listdir(pathStr)
-    files = [*filter(
-            lambda i: path.isfile(i),
-            [*map(
-                    lambda i: str(Path(pathStr, i)),
-                    filesAndFolders
-            )]
-    )]
-    folders = [*filter(
-            lambda i: path.isdir(i),
-            [*map(
-                    lambda i: str(Path(pathStr, i)),
-                    filesAndFolders
-            )]
-    )]
-    return files, folders
-
-
-
-
-def is_exists(path_obj):
+def get_all(path_str, classify=False, nopath=False):
     """
-    判断传入的路径是否存在
+    返回给定路径的直接文件列表和文件夹列表
+    默认返回一个列表包含所有的文件和文件夹
+    classify为True就返回两个列表,一个是文件列表一个是目录列表,默认的False表示返回一个列表
     """
-    pass
+    files = []
+    dirs = []
 
+    if not path_str:
+        path_str = cwd()
 
-def mkdir(path_obj):
-    """
-    新建一个目录
-    """
-    pass
+    for item in listdir(path_str):
+        item_path = Path(path_str, item)
+        final_name = item if nopath else str(item_path)
+        if is_file(item_path):
+            files.append(final_name)
+        else:
+            dirs.append(final_name)
 
-def rmdir(path_obj):
-    """
-    删除一个目录
-    """
-    pass
+    if classify:
+        return files, dirs
+    else:
+        return [*files, *dirs]
--- pathx.py
+++ pathx.py
@@ -7,7 +7,7 @@ is_dir = path.isdir # 判断是否是文件夹
 is_link = is_symlink = path.islink # 重命名判断软链接的方法
 is_exist = path.exists # 判断路径是否存在
 
-def cwd(path_str):
+def cwd(path_str=None):
     """
     获取或者设置当前的工作目录
     如果给出了 path_str 那么就是设置 没有给出就是获取当前的工作目录
