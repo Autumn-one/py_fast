484a8724705f21f59da859550d55ea9204d67342
Autumn-one <718879459@qq.com>
2022-02-06 13:03:02 +0800

    添加一些文件的高级操作

--- osx.py
+++ osx.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from pathx import *  # 导入所有的路径增强函数
 from walkx import *  # 导入所有的递归访问文件夹的方法
 from pprint import *  # 导入所有的pprint的方法
-from shutil import * # 导入文件的高级操作部分
+from shutil import *  # 导入文件的高级操作部分
 import json
+import ast
 
 
 def get_dirs(path_str=None, nopath=False):
@@ -78,7 +79,8 @@ def get_all(path_str, classify=False, nopath=False):
 # 创建一个目录
 new_dir = os.mkdir
 
-def new_file(file_name,content=None,encoding="utf8"):
+
+def new_file(file_name, content=None, encoding="utf8"):
     """
     创建一个文件
     file_name 文件名称或者一个文件路径
@@ -86,7 +88,7 @@ def new_file(file_name,content=None,encoding="utf8"):
     encoding是文字编码
     """
     if content:
-        Path(file_name).write_text(content,encoding=encoding)
+        Path(file_name).write_text(content, encoding=encoding)
     else:
         Path(file_name).touch()
 
@@ -101,50 +103,62 @@ def remove(file_dir):
     else:
         rmtree(file_dir)
 
+
 # 删除文件
 remove_file = os.remove
 
 # 删除文件夹
 remove_dir = rmtree
 
-def read_file(file_name,encoding="utf8"):
+stat = os.stat
+
+
+def read(file_name, encoding="utf8"):
     """
     读取文件内容
     """
-    with open(file_name,encoding=encoding) as f:
+    with open(file_name, encoding=encoding) as f:
         return f.read()
 
-def write_file(file_name,content,*,append=True,encoding="utf8"):
+
+def write(file_name, content, *, append=True, encoding="utf8"):
     """
     写入文件内容
     append如果为True则是追加写入 False则是覆写
     """
-    mode = "a" if append else "w" # 写入的模式是追加还是覆写
-    with open(file_name,mode,encoding=encoding) as f:
+    mode = "a" if append else "w"  # 写入的模式是追加还是覆写
+    with open(file_name, mode, encoding=encoding) as f:
         f.write(content)
 
-def read_json():
+
+def read_json(path_str):
     """
     读取json文件返回的是一个py对象
     """
-    pass
+    with open(path_str, encoding="utf8") as f:
+        return json.load(f)
 
-def write_json():
+
+def write_json(path_str, obj):
     """
     将py对象写入到json文件
     """
-    pass
+    with open(path_str, "w", encoding="utf8") as f:
+        return json.dump(obj, f, check_circular=False)
 
-def str2obj():
+
+def str2obj(s):
     """
     字符串转换成py对象并返回
     """
-    pass
+    return ast.literal_eval(s)
 
-def obj2str():
+
+def obj2str(obj):
     """
     py对象转换成字符串
     """
-    pass
+    return json.dumps(obj, check_circular=False)
+
 
 copy_dir = copytree
